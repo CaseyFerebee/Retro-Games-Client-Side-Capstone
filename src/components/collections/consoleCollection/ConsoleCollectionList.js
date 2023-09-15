3ef72b9efc266c7daa3d4857315954e19c38d99c
Casey Ferebee <cwferebee@gamil.com>
2023-09-15 14:52:57 -0500

    added params to console_collection view

---
+++ src/components/collections/consoleCollection/ConsoleCollectionList.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react"
+import { Link } from 'react-router-dom'
+import { getCurrentOwnerConsoleCollection } from "../../../managers/ConsoleCollectionManager"
+
+export const ConsoleCollectionList = () => {
+
+  const [consoleCollections, setConsoleCollections] = useState([])
+
+
+  useEffect(
+    () => {
+      getCurrentOwnerConsoleCollection().then((consoleData) => setConsoleCollections(consoleData))
+    },
+    []
+  )
+
+  return (
+    <div className="container">
+      <h1 className="console-collection-title">Console Collection</h1>
+      <article className="">
+        {consoleCollections.map(
+          (consoleObject) => {
+            return <div className="console" key={consoleObject.id}  >
+              <div className="title"><Link to={`/consoles/${consoleObject?.console?.id}`} key={consoleObject.id}>{consoleObject?.console?.name}</Link></div>
+              <section className="" >
+                <img src={consoleObject?.console?.img} alt={consoleObject?.console?.name} />
+              </section>
+            </div>
+          })}
+      </article >
+    </div >
+  )
+}
+
